c05faac1fe9942503e395a0ba72e88d941a814b6
Balozmen <152926015+Balozmen@users.noreply.github.com>
2026-05-13 23:20:32 +0100

    Initialize Express app and setup WhatsApp bot

---
+++ index.js const {   default: makeWASocket,   useMultiFileAuthState, } = require("@whiskeysockets/index.js
@@ -0,0 +1,45 @@
+index.js
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+} = require("@whiskeysockets/baileys");
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info");
+
+  const sock = makeWASocket({
+    auth: state,
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0];
+
+    if (!msg.message) return;
+
+    const text =
+      msg.message.conversation ||
+      msg.message.extendedTextMessage?.text;
+
+    if (text === "ping") {
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "pong 🏓",
+      });
+    }
+  });
+}
+
+startBot(
